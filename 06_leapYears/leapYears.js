eae46e0a869fa4ac95509d2e8f3441b2eae9767d
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-10 13:39:40 -0700

    Saves answers to exercises 05 - 07

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  console.log(year % 4);
+  console.log(year % 400);
+  console.log(year % 100);
+  if 
+    (year % 400 === 0 && year % 4 === 0) {  
+      return true;
+  } else if 
+    (year % 4 === 0 && year % 100 !== 0) {
+      return true;
+  } else {
+      return false;
+  }
 };
+
+console.log(leapYears(1997));
 
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let fConv = ((f - 32) * 5 / 9);
+  if (fConv % 1 !== 0) {
+    fConv = ((f - 32) * 5 / 9).toFixed(1)
+  }
+  return parseFloat(fConv);
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let cConv = ((c / 5 * 9) + 32);
+  if (cConv % 1 !== 0) {
+    cConv = ((c / 5 * 9) + 32).toFixed(1);
+  }
+  return parseFloat(cConv);
 };
 
 // Do not edit below this line
