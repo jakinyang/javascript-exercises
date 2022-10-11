c5390feb05a0ef817921e64c130473f5f0263ce8
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-10 18:47:22 -0700

    Saves answers to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num < 0) {
+    return 'OOPS';
+  }
+  let initialValue = 0;
+  let fibArray = [1]
+    for (let i = 0; i < num; i++) {
+      if (fibArray[i]) {
+        i++
+      }
+        fibArray[i] = initialValue + fibArray[i-1];
+        initialValue = fibArray[i-1];
+    }
+  return fibArray[num-1];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
