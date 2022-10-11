5a0f7b39be8d710bb4d525895bd55d38f07bbff8
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-10 17:18:03 -0700

    Submits answers to exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  // For consistency across letter cases
+  word = word.toLowerCase();
+  let regex = /\W+/g;
+  word = word.replace(regex, '');
+  console.log(word);
 
+  let j = word.length -1;  
+  for (let i = 0; i < word.length; i++) {
+    if(word[j] !== word[i]) {
+        return false;
+      }
+    if (j >= 0) {
+      j--;
+    }
+  }
+  return true;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
