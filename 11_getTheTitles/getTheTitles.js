340fe70966314316179d67244447a8a440fbe412
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-10 21:20:35 -0700

    Saves changes to exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+  let titleArr = new Array;
+  array.forEach(item => {
+    titleArr.push(item.title);
+  })
+  return titleArr;
 };
 
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+
+console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
