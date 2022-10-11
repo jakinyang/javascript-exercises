f480185d47b6c82bdf562e61a6ff9e6bb626fc6b
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-10 21:20:11 -0700

    Saves answers to exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(array) {
+  array.forEach(person => {
+    if(!person.yearOfDeath) {
+      let date = new Date();
+      person.yearOfDeath = date.getFullYear();
+    }
+    person.age = person.yearOfDeath - person.yearOfBirth;
+  })
+  array.sort((a, b) => b.age - a.age);
+  return array[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
